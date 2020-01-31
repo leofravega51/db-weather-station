742d965aaa7544e57ffa2b6a7bb1040134889a96
Leandro <leo-fravega@hotmail.com>
2020-01-31 07:52:23 -0300

    feat(db): Database creation script

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE deashvehcv90hh
+    WITH 
+    OWNER = hapzwcekisrziq
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.UTF-8'
+    LC_CTYPE = 'en_US.UTF-8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+GRANT ALL ON DATABASE deashvehcv90hh TO hapzwcekisrziq;
