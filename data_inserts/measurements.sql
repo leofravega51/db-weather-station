a096c068a6e2b5ac1aac1f4338d96205a9bb235b
Leandro <leo-fravega@hotmail.com>
2020-01-31 07:48:15 -0300

    feat(measurement script): Measurement data insertion script

---
+++ data_inserts/measurements.sql
@@ -0,0 +1,32 @@
+SELECT registerMeasurement(21.22, 67.96, 32.59, 38.88, 87.93, 89.63, 10.0, 9, 'BASE LARROQUE');
+SELECT registerMeasurement(11.01, 29.53, 77.91, 79.99, 50.65, 39.71, 4.0, 67, 'BASE CONCEPCION DEL URUGUAY');
+SELECT registerMeasurement(1.89, 62.32, 43.99, 55.32, 34.80, 73.28, 96.0, 60, 'BASE GUALEGUAYCHU');
+SELECT registerMeasurement(14.45, 7.88, 61.73, 56.51, 89.79, 53.53, 24.0, 15, 'BASE CONCORDIA');
+SELECT registerMeasurement(47.58, 16.73, 21.56, 41.36, 34.68, 20.97, 40.0, 69, 'BASE CORRIENTES');
+SELECT registerMeasurement(49.99, 63.26, 67.07, 61.40, 91.12, 16.97, 4.0, 14, 'BASE BARRIO ALTO PALERMO');
+SELECT registerMeasurement(24.72, 90.18, 32.17, 76.49, 97.23, 51.46, 65.0, 19, 'BASE VILLAGUAY');
+SELECT registerMeasurement(-14.46, 98.95, 24.22, 72.12, 53.10, 59.44, 95.0, 35, 'BASE BASAVILBASO');
+SELECT registerMeasurement(2.75, 36.84, 42.81, 37.00, 16.13, 90.18, 77.0, 41, 'BASE SANTA FE');
+SELECT registerMeasurement(-3.83, 70.17, 21.39, 13.41, 3.19, 62.78, 11.0, 74, 'BASE PARANA');
+
+SELECT registerMeasurement(21.22, 67.96, 32.59, null, 87.93, 89.63, 10.0, 9, 'BASE LARROQUE');
+SELECT registerMeasurement(11.01, 29.53, 77.91, 79.99, 50.65, null, 4.0, 67, 'BASE CONCEPCION DEL URUGUAY');
+SELECT registerMeasurement(1.89, 62.32, 43.99, 55.32, 34.80, 73.28, null, 60, 'BASE GUALEGUAYCHU');
+SELECT registerMeasurement(14.45, 7.88, null, null, 89.79, 53.53, 24.0, 15, 'BASE CONCORDIA');
+SELECT registerMeasurement(47.58, 16.73, 21.56, 41.36, 34.68, 20.97, 40.0, null, 'BASE CORRIENTES');
+SELECT registerMeasurement(null, 63.26, 67.07, 61.40, 91.12, 16.97, 4.0, 14, 'BASE BARRIO ALTO PALERMO');
+SELECT registerMeasurement(24.72, null, 32.17, 76.49, 97.23, 51.46, 65.0, 19, 'BASE VILLAGUAY');
+SELECT registerMeasurement(-14.46, 98.95, 24.22, 72.12, 53.10, 59.44, 95.0, null, 'BASE BASAVILBASO');
+SELECT registerMeasurement(2.75, null, 42.81, null, 16.13, 90.18, 77.0, 41, 'BASE SANTA FE');
+SELECT registerMeasurement(-3.83, 70.17, 21.39, 13.41, 3.19, null, 11.0, 74, 'BASE PARANA');
+
+SELECT registerMeasurement(21.22, 67.96, 32.59, 38.88, 87.93, 89.63, 10.0, 9, 'BASE LARROQUE');
+SELECT registerMeasurement(11.01, 29.53, 77.91, 79.99, 50.65, 39.71, 4.0, 67, 'BASE CONCEPCION DEL URUGUAY');
+SELECT registerMeasurement(1.89, 62.32, 43.99, 55.32, 34.80, 73.28, 96.0, 60, 'BASE GUALEGUAYCHU');
+SELECT registerMeasurement(14.45, 7.88, 61.73, 56.51, 89.79, 53.53, 24.0, 15, 'BASE CONCORDIA');
+SELECT registerMeasurement(47.58, 16.73, 21.56, 41.36, 34.68, 20.97, 40.0, 69, 'BASE CORRIENTES');
+SELECT registerMeasurement(49.99, 63.26, 67.07, 61.40, 91.12, 16.97, 4.0, 14, 'BASE BARRIO ALTO PALERMO');
+SELECT registerMeasurement(24.72, 90.18, 32.17, 76.49, 97.23, 51.46, 65.0, 19, 'BASE VILLAGUAY');
+SELECT registerMeasurement(-14.46, 98.95, 24.22, 72.12, 53.10, 59.44, 95.0, 35, 'BASE BASAVILBASO');
+SELECT registerMeasurement(2.75, 36.84, 42.81, 37.00, 16.13, 90.18, 77.0, 41, 'BASE SANTA FE');
+SELECT registerMeasurement(-3.83, 70.17, 21.39, 13.41, 3.19, 62.78, 11.0, 74, 'BASE PARANA');
