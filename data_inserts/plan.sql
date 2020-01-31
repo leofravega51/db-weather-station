102289ee1862c52e423d0687d16598ac9890628c
Leandro <leo-fravega@hotmail.com>
2020-01-31 07:48:37 -0300

    feat(plan script): Plan data insertion script

---
+++ data_inserts/plan.sql
@@ -0,0 +1,3 @@
+INSERT INTO plan(description, price, amount_consults) VALUES('BASIC', 0, 50);
+INSERT INTO plan(description, price, amount_consults) VALUES('INTERMEDIATE', 5.00, 300);
+INSERT INTO plan(description, price, amount_consults) VALUES('PREMIUM', 15.00, 2147483647);
